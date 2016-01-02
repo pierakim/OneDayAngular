7c420f5aa2f5eacf5eac5c9d6ed6f03e74e3cd1b
pierakim <pierakim.lambda@gmail.com>
2016-01-03 00:21:56 +0100

    refactoring file name

--- javascript/CtrlBinding.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Chicken on 02/01/2016.
- */
-myApp.controller('CtrlBinding', ['$scope', function ($scope) {
-    // capture data model
-    // empty string init
-    $scope.myModel = '';
-}]);
